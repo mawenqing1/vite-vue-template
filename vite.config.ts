7460e6a10abfd62684983821b0e5e8b8155ae524
马文卿 <529837505@qq.com>
2022-11-01 15:32:36 +0800

    add less

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { normalizePath } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+const variablePath = normalizePath(path.resolve('./src/assets/variable.less'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+		port: 5000,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		},
+	},
+  css: {
+    preprocessorOptions: {
+			less: {
+				javascriptEnabled: true,
+				additionalData: `@import "${variablePath}";`
+			},
+		}
+  },
   plugins: [vue()]
 })
